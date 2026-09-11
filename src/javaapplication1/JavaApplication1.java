fd107c4dd860bc62f9dae332d35b627aa26a45e0
SEBASTIAN <SEBASTIAN@192.168.1.83>
2026-09-11 13:22:09 -0300

    proyecto avanzado,agregacion de ventana swing y creo que guardado de archivos

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -1,24 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package javaapplication1;
 
-/**
- *
- * @author SEBASTIAN
- */
+import controlador.GestorHospital;
+import vista.MenuConsola;
+import vista.VentanaPrincipal;
+
+import javax.swing.SwingUtilities;
+
 public class JavaApplication1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
 
-        System.out.println("primer commit exitoso");
+        GestorHospital gestor = new GestorHospital();
+
+        // Lanzar interfaz gráfica Swing
+        SwingUtilities.invokeLater(() -> {
+            VentanaPrincipal ventana = new VentanaPrincipal(gestor);
+            ventana.setVisible(true);
+        });
+
+        // Lanzar consola interactiva
+        MenuConsola menu = new MenuConsola(gestor);
+        menu.iniciar();
     }
-
 }
